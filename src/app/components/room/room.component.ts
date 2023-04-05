c0529bda315e69700582483f723037c1c35af3cd
KACHHAP LILY <99547353+kachhaplily@users.noreply.github.com>
2023-04-05 16:19:29 +0530

    built in direct *ngif ,*ngfor,*ngclass ,*ngswitch

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,4 +1,5 @@
 import { Component ,OnInit} from '@angular/core';
+import { room, roomlist } from './room';
 
 @Component({
   selector: 'app-room',
@@ -9,8 +10,56 @@ export class RoomComponent implements OnInit{
 
    hotelName:string="hilton hotel";
    numberroom:number=20;
+   hideroom=false;
+
+   Room:room={
+    totalroom:20,
+    avl:12,
+    bookedroom:5
+   }
+
+  Roomlist:roomlist[]=[
+    {
+      roomnumber:1,
+    roomtype:"delux room",
+    ameninites:"air conditioner,free wifi,breakfast",
+    price:500,
+    photos:"https://images.unsplash.com/photo-1679678690998-88c8711cbe5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
+    checkin: new Date ('05-04-2023'),
+    checkout: new Date ('06-04-2034')
+
+  },
+  {
+    roomnumber:2,
+
+    roomtype:"delux room",
+    ameninites:"air conditioner,free wifi,breakfast,free water",
+    price:1200,
+    photos:"https://images.unsplash.com/photo-1614649024145-7f847b1c803f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    checkin: new Date ('06-04-2023'),
+    checkout: new Date ('08-04-2034')
+
+  },
+  {
+    roomnumber:4,
+
+    roomtype:"delux room private",
+    ameninites:"air conditioner,free wifi,breakfast",
+    price:500,
+    photos:"https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    checkin: new Date ('05-04-2023'),
+    checkout: new Date ('06-04-2034')
+
+  },
+
+]
+
+   constructor(){}
   ngOnInit(): void {
 
   }
+   toggle(){
+    this.hideroom=!this.hideroom
+   }
 
 }
---
+++ src/app/components/room/room.ts
@@ -0,0 +1,16 @@
+export interface room{
+  totalroom:number;
+  avl:number;
+  bookedroom:number;
+}
+
+
+export interface roomlist{
+  roomnumber:number;
+  roomtype:string;
+  ameninites:string;
+  price:number;
+  photos:string;
+  checkin:Date;
+  checkout:Date;
+}
